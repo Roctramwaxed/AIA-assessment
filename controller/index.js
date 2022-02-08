31867deb4973a309e580a5f20121c837f5ce43ee
roctramwaxed <izra.arzi@gmail.com>
2022-02-08 23:53:59 +0700

    api get

--- controller/index.js
+++ controller/index.js
@@ -1,6 +1,26 @@
-const axios = require('axios')
+const axios = require("axios");
+const convert = require("xml-js");
 
 class Controller {
+  static async getImageList(req, res) {
+    try {
+      let url = "https://www.flickr.com/services/feeds/photos_public.gne";
+      if (req.body.tags.length) {
+        url += `?tags=${req.body.tags.split(" ").join(",")}`;
+      }
+
+      const response = await axios.get(url);
+      let data = convert.xml2js(response.data, {
+        compact: true,
+        spaces: 2,
+      });
+      data = data.feed.entry.slice(0, 9);
+      res.status(200).send(data);
+    } catch (err) {
+      console.log("error:", err);
+      res.status(500).send(err);
+    }
+  }
 }
 
-module.exports = Controller
+module.exports = Controller;
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 const route = require("express").Router();
 const Controller = require("../controller");
 
+route.get("/images", Controller.getImageList);
+
 module.exports = route;
